a92fe8492069e0323238c681777f8bb8a6e08dd7
farhod <farhod@4-lab.com>
2024-05-19 00:02:13 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Farkhod on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
